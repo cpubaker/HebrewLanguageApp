58880af0361a4cbf19ab14c85a99564b7edb1a64
Yevhen Nedashkivskyi <cpubaker@gmail.com>
2026-04-25 16:04:53 +0300

    feat: update main function to support asynchronous initialization and set preferred orientations

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 import 'app.dart';
 import 'services/theme_mode_store.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(const [
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     const HebrewFlutterApp(themeModeStore: SharedPreferencesThemeModeStore()),
   );
